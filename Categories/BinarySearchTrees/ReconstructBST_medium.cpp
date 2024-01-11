881af980a5442671f363bb11a3c45dfeb4533c4b
Ben Ollomo <81216157+bbenjii@users.noreply.github.com>
2024-01-11 12:07:09 -0500

    Create ReconstructBST_medium.cpp

---
+++ Categories/BinarySearchTrees/ReconstructBST_medium.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+// This is an input class. Do not edit.
+class BST {
+ public:
+  int value;
+  BST* left = nullptr;
+  BST* right = nullptr;
+
+  BST(int value) { this->value = value; }
+};
+
+BST* reconstructBst(vector<int> preOrderTraversalValues) {
+  // Write your code here.
+  return nullptr;
+}
