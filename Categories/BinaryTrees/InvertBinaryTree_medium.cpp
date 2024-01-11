dbb0a84303e775072593387b9f28d381bd1107c7
Ben Ollomo <81216157+bbenjii@users.noreply.github.com>
2024-01-11 12:10:00 -0500

    Create InvertBinaryTree_medium.cpp

---
+++ Categories/BinaryTrees/InvertBinaryTree_medium.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class BinaryTree {
+public:
+  int value;
+  BinaryTree *left;
+  BinaryTree *right;
+
+  BinaryTree(int value);
+  void insert(vector<int> values, int i = 0);
+  void invertedInsert(vector<int> values, int i = 0);
+};
+
+void invertBinaryTree(BinaryTree *tree) {
+  // Write your code here.
+  
+    if(tree == nullptr) return;
+
+    BinaryTree *temp = tree->left;
+    tree->left = tree->right;
+    tree->right = temp;
+
+    invertBinaryTree(tree->left);
+    invertBinaryTree(tree->right);
+}
